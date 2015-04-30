ae457f1dc330d130814124c170e8c77e198f6dfb
PAEz <cdkpaez@gmail.com>
2015-04-30 18:22:46 +0930

    change save back
    Commented out the save test

--- javascripts/fileExporter - Copy.js
+++ javascripts/fileExporter - Copy.js
@@ -1,78 +1,92 @@
-// dummy locate to keep onFrame happy until one is initiated
 var locate = {
 	scan: {
 		scanning: false
 	}
 };
-
 var $ = {
-	progress: {
-		el: document.querySelector('#progress'),
-		value: 0
+	rootList: document.querySelector('#rootlist'),
+	// load: document.querySelector('#load'),
+	fileInput: document.querySelector('#fileinput'),
+	// generate: document.querySelector('#generate'),
+	// save: document.querySelector('#save'),
+	progress: document.querySelector('#progress'),
+	template: {
+		container: document.querySelector('div.template'),
+		header: document.querySelector('div.template [name="header"]'),
+		root: document.querySelector('div.template [name="root"]'),
+		file: document.querySelector('div.template [name="file"]'),
+		directory: document.querySelector('div.template [name="directory"]'),
+		// dirend: document.querySelector('div.template [name="dirend"]'),
+		// rootend: document.querySelector('div.template [name="rootend"]'),
+		footer: document.querySelector('div.template [name="footer"]'),
 	},
+	legend: {
+		container: document.querySelector('div.legend'),
+		root: document.querySelector('div.legend .root'),
+		directory: document.querySelector('div.legend .directory'),
+		file: document.querySelector('div.legend .file'),
+		end: document.querySelector('div.legend .end'),
+	}
+}
+
+var gui = {
 	load: {
-		el: document.querySelector('#load'),
+		el:document.querySelector('#load'),
 		enabled: true
 	},
 	generate: {
-		el: document.querySelector('#generate'),
+		el:document.querySelector('#generate'),
 		enabled: false
 	},
 	save: {
-		el: document.querySelector('#save'),
+		el:document.querySelector('#save'),
 		enabled: false
 	},
-	rootList: document.querySelector('#rootlist'),
-	fileInput: document.querySelector('#fileinput'),
-	template: document.querySelector('div.template'),
-	legend: document.querySelector('div.legend')
 };
-
-var $Keys = Object.keys($);
-$Keys.forEach(function(key) {
-	var el = $[key];
-	if (el.enabled !== undefined) {
-		if (el.enabled) {
-			el.el.removeAttribute('disabled');
-		} else {
-			el.el.setAttribute('disabled', '');
-		}
-		el.enabled = function(bool) {
-			if (bool === undefined) bool = true;
-			if (bool) {
-				this.el.removeAttribute('disabled');
-			} else {
-				this.el.setAttribute('disabled', '');
-			}
-		}
-	}
+gui.progress = 0;
+gui.progressLast = 0;
+var guiKeys = Object.keys(gui);
+guiKeys.forEach(function(key){
+	gui[key].enabledLast=!gui[key].enabled;
 })
 
 
 function onFrame() {
-	if ($.progress.value !== $.progress.valueLast) {
-		$.progress.el.style.width = $.progress.value + '%';
-		$.progress.valueLast = $.progress.value;
-	} else if (locate.scan.scanning == false) {
+	// if(locate.scan.scanning){
+	if (gui.progress !== gui.progressLast) {
+		$.progress.style.width = gui.progress + '%';
+		gui.progressLast = gui.progress;
+		// }
+	} else {
 		var update = false;
 		for (var i = 0, iEnd = templateKeys.length; i < iEnd; i += 1) {
 			var key = templateKeys[i];
 			if (template[key].dirty) {
 				template[key].dirty = false;
-				templateData[key] = template[key].el.value;
+				templateData[key] = $.template[key].value;
 				update = true;
 			}
+			// if(gui[])
 		}
 		if (update) {
 			localStorage['templateData'] = JSON.stringify(templateData);
 		}
 	}
+	for (var i = 0, iEnd = guiKeys.length; i < iEnd; i += 1) {
+		var key = guiKeys[i];
+		if (gui[key].enabled != gui[key].enabledLast) {
+			if (gui[key].enabled) {
+				gui[key].el.removeAttribute('disabled');
+			} else {
+				gui[key].el.setAttribute('disabled', '');
+			}
+		}
+		gui[key].enabledLast = gui[key].enabled;
+	}
 	window.requestAnimationFrame(onFrame);
 }
 
 window.requestAnimationFrame(onFrame);
-
-
 
 $.fileInput.addEventListener('change', function(e) {
 	var files = e.target.files || e.dataTransfer.files;
@@ -80,8 +94,10 @@ $.fileInput.addEventListener('change', function(e) {
 		var file = files[0];
 		var reader = new FileReader();
 		rootListInfo('Loading......');
-		$.generate.enabled(false);
-		$.save.enabled(false);
+		gui.generate.enabled=false;
+		gui.save.enabled=false;
+		// $.generate.setAttribute('disabled', '');
+		// $.save.setAttribute('disabled', '');
 		reader.readAsArrayBuffer(file);
 		reader.onload = function(result) {
 			var ab = result.target.result;
@@ -90,21 +106,22 @@ $.fileInput.addEventListener('change', function(e) {
 				rootListInfo(locate.error);
 				return;
 			}
-			$.generate.enabled(true);
+			// $.generate.removeAttribute('disabled');
+			gui.generate.enabled = true;
 			rootList(locate.roots);
 		}
 	}
 
 })
 
-$.save.el.addEventListener('click', function(e) {
+gui.save.el.addEventListener('click', function(e) {
+
 	save(list);
 });
 // the generate click event is at the end of the code
 
 var template = {
 	header: {
-		el:document.querySelector('div.template [name="header"]'),
 		dirty: false,
 		func: null,
 		args: 'item, date, dateRaw',
@@ -126,7 +143,6 @@ var template = {
 		}
 	},
 	root: {
-		el:document.querySelector('div.template [name="root"]'),
 		dirty: false,
 		func: null,
 		args: 'item, date, dateRaw',
@@ -152,7 +168,6 @@ var template = {
 		}
 	},
 	directory: {
-		el:document.querySelector('div.template [name="directory"]'),
 		dirty: false,
 		func: null,
 		args: 'item, path, date, dateRaw',
@@ -186,7 +201,6 @@ var template = {
 		}
 	},
 	file: {
-		el:document.querySelector('div.template [name="file"]'),
 		dirty: false,
 		func: null,
 		args: 'item, path, date, dateRaw',
@@ -245,7 +259,6 @@ var template = {
 	//   }
 	// },
 	footer: {
-		el:document.querySelector('div.template [name="footer"]'),
 		dirty: false,
 		func: null,
 		args: 'item, date, dateRaw',
@@ -271,10 +284,15 @@ var template = {
 var templateKeys = Object.keys(template);
 
 templateKeys.forEach(function(legend) {
-	var el = template[legend].el;
+	var el = $.template[legend];
+
+	// gui[legend] = {
+	// 	enabled: false,
+	// 	enabledLast: false
+	// }
 
 	el.addEventListener('focus', function(e) {
-		$.legend.setAttribute('legend', el.getAttribute('name'));
+		$.legend.container.setAttribute('legend', el.getAttribute('name'));
 	});
 
 	el.addEventListener('input', function(e) {
@@ -287,7 +305,7 @@ var templateData = {};
 if (localStorage['templateData'] !== undefined) templateData = JSON.parse(localStorage['templateData']);
 
 templateKeys.forEach(function(key) {
-	if (templateData[key]) template[key].el.value = templateData[key];
+	if (templateData[key]) $.template[key].value = templateData[key];
 })
 
 
@@ -295,7 +313,7 @@ templateKeys.forEach(function(key) {
 function createTemplate(section, str) {
 	var args = template[section].args;
 	var keys = template[section].keys;
-	var undefinedKeys = [];
+	this.undefinedKeys = [];
 	str = str.replace(/(?:\r\n|\r|\n)/g, '\r\n')
 	str = JSON.stringify(str);
 
@@ -313,10 +331,10 @@ function createTemplate(section, str) {
 function createTemplates(templates) {
 	var keys = Object.keys(templates);
 	for (var i = 0, iEnd = keys.length; i < iEnd; i += 1) {
-		var key = keys[i];
-		var temp = createTemplate(key, template[key].el.value.toString());
+		var template = keys[i];
+		var temp = createTemplate(template, $.template[template].value.toString());
 		if (temp !== true) {
-			temp.template = key;
+			temp.template = template;
 			return temp;
 		}
 	}
@@ -337,8 +355,8 @@ function rootList(roots) {
 		volumeLabel = document.createElement('label');
 		volumeLabel.classList.add('checkbox');
 		volumeLabel.setAttribute('for', 'volume' + index);
-		volumeLabel.innerText = root.path + '\\';
-		volumeLabel.setAttribute('title', root.path + '\\');
+		volumeLabel.innerText = root.path + '/';
+		volumeLabel.setAttribute('title', root.path + '/');
 		var info1 = document.createElement('span');
 		info1.classList.add("info");
 		info1.appendChild(document.createTextNode("Files: "));
@@ -371,6 +389,23 @@ function rootListInfo(wot) {
 	li.appendChild(text);
 	$.rootList.appendChild(li);
 }
+
+// Object.keys($.template).forEach(function(legend) {
+// 	if (legend != 'container') {
+// 		var el = $.template[legend];
+
+// 		el.addEventListener('focus', function(e) {
+// 			$.legend.container.setAttribute('legend', el.getAttribute('name'));
+// 		});
+
+// 		el.addEventListener('input', function(e) {
+// 			// $.legend.container.setAttribute('legend', el.getAttribute('name'));
+// 			// console.log(el.name)
+// 			template[el.name].dirty = true;
+// 		});
+// 	}
+// })
+
 
 
 function createDates(item) {
@@ -435,42 +470,24 @@ function save(wot, filename) {
 		t = 'application/binary';
 	a.onclick = function() {
 		URL.revokeObjectURL(blob);
-		delete blob;
 	};
 	a.download = filename;
 	a.href = URL.createObjectURL(blob);
+	a.dataset.downloadurl = t + ':' + a.download + ':' + a.href;
 	e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
 	a.dispatchEvent(e)
 }
-// Well that didnt help ;)
-// function save(wot, filename) {
-// 	var enc=new TextEncoder('utf8');
-// 	filename = filename || 'output.txt';
-// 	var blob = new Blob([enc.encode(wot)], {
-// 			type: t
-// 		}),
-// 		e = document.createEvent('MouseEvents'),
-// 		a = document.createElement('a'),
-// 		t = 'application/binary';
-// 	a.onclick = function() {
-// 		URL.revokeObjectURL(blob);
-// 		delete blob;
-// 	};
-// 	a.download = filename;
-// 	a.href = URL.createObjectURL(blob);
-// 	e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
-// 	a.dispatchEvent(e)
-// }
+
 
 
 rootListInfo('Load a DB to get started');
 var list = '';
 
-$.generate.el.addEventListener('click', function(e) {
+gui.generate.el.addEventListener('click', function(e) {
 	var totals = 0,
-		totalsCount = 0,
-		volumes = [];
-// for(var i=0;i<3;i++){
+		totalsCount = 0;
+	var volumes = [];
+
 	[].forEach.call($.rootList.querySelectorAll('input[type="checkbox"]'), function(el) {
 		if (el.checked) {
 			var v = el.getAttribute('volume');
@@ -478,15 +495,15 @@ $.generate.el.addEventListener('click', function(e) {
 			totals += locate.roots[v].directories;
 			volumes.push(v);
 		}
-	});
-// }
+	})
+
 	if (!volumes.length) return;
-	volumes.reverse();
-
-	$.save.enabled(false);
-	$.load.enabled(false);
-	$.generate.enabled(false);
-
+	gui.save.enabled=false;
+	gui.load.enabled=false;
+	gui.generate.enabled=false;
+	// $.save.setAttribute('disabled', '');
+	// $.load.setAttribute('disabled', '');
+	// $.generate.setAttribute('disabled', '');
 	list = '';
 
 	totalsCount = 0;
@@ -504,35 +521,66 @@ $.generate.el.addEventListener('click', function(e) {
 
 		function scanIt(index) {
 			createDates(locate.roots[index]);
-			list += template.root.func(locate.roots[index], date, dateRaw);
+			list += template['root'].func(locate.roots[index], date, dateRaw);
 
 			locate.roots[index].scan(function(item, path) {
 				if (item.event) {
-					if (item.event == 'end') {
-						var next = volumes.pop();
-						if (next) scanIt(next);
-						else {
-							// true end
-							console.timeEnd('Scan Took');
-							console.log('totals', totals)
-							list += template['footer'].func(locate.header, date, dateRaw);
-							$.save.enabled(true);
-							$.load.enabled(true);
-							$.generate.enabled(true);
-							$.progress.value = 0;
-						}
-						return;
+
+					switch (item.event) {
+
+						case 'end':
+							var next = volumes.pop();
+							if (next) scanIt(next);
+							else {
+								// true end
+								console.timeEnd('Scan Took');
+								console.log('totals', totals)
+								list += template['footer'].func(locate.header, date, dateRaw);
+								gui.save.enabled=true;
+	gui.load.enabled=true;
+	gui.generate.enabled=true;
+								// $.save.removeAttribute('disabled');
+								// $.load.removeAttribute('disabled');
+								// $.generate.removeAttribute('disabled');
+								gui.progress = 0;
+							}
+							return;
+							// console.log(files)
+							break;
+
+						case 'pause':
+							console.log('Scan Paused');
+							console.timeEnd('Amount of time to pause');
+							break;
+
+						case 'continue':
+							console.log('Scan Continuing');
+							break;
+
+						case 'updir':
+							/*
+                This event is called whenever we leave a directory and are back in its parent
+                You can use this with the rest to reconstruct full trees if you want
+              */
+							// console.log('updir');
+							break;
 					}
 				} else if (item.attrs.directory) {
 					createDates(item);
-					list += template.directory.func(item, path, date, dateRaw);
+					list += template['directory'].func(item, path, date, dateRaw);
 					totalsCount += 1;
-					$.progress.value = (totalsCount / totals) * 100;
+					gui.progress = (totalsCount / totals) * 100;
+
+					// testy = path.match(/.*[a-zA-Z].*[0-9]/); // just a useless regex to make it do something each item
+					// testy = item.name.match(/.*[a-zA-Z].*[0-9]/); // just a useless regex to make it do something each item
+					// files+=' '+path+item.name;
 				} else {
 					createDates(item);
+					// testy = item.name.match(/.*[a-zA-Z].*[0-9]/); // just a useless regex to make it do something each item
+					// files+=' '+path+item.name;
 					totalsCount += 1;
-					list += template.file.func(item, path, date, dateRaw);
-					$.progress.value = (totalsCount / totals) * 100;
+					list += template['file'].func(item, path, date, dateRaw);
+					gui.progress = (totalsCount / totals) * 100;
 				}
 			});
 		}
@@ -541,9 +589,12 @@ $.generate.el.addEventListener('click', function(e) {
 
 	} else {
 		alert('Unable to generate template.\nUnkown variable/s...\n' + temp.join(', ') + '\n...was used in...\n' + temp.template);
-		$.save.enabled(false);
-		$.load.enabled(true);
-		$.generate.enabled(true);
+		gui.save.enabled=false;
+	gui.load.enabled=true;
+	gui.generate.enabled=true;
+		// $.save.setAttribute('disabled', '');
+		// $.load.removeAttribute('disabled');
+		// $.generate.removeAttribute('disabled');
 	}
 
 });
